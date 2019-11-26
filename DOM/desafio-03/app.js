a429696b610322d8a75e2423bbc868b927c430cd
gbmatheus <gabrielmatheus2503@gmail.com>
2019-11-26 09:24:29 -0300

    Desafio 03, propriedades reativas

---
+++ DOM/desafio-03/app.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0
+    },
+    computed: {
+        resultado() {
+            return this.valor == 37 ? 'Valor igual' : 'Valor diferente'
+        },
+    },
+    watch: {
+        resultado(novo, antigo) {
+            if(novo)
+            setTimeout(() => {
+                this.valor=0
+            }, 5000);
+        }
+    },
+});
